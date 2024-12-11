a27ec99163aa48e68a6779c618c48a15bf9f52cc
Lucas Labre <lucaslabre@gmail.com>
2024-12-11 19:02:02 -0300

    Criação da página de visualização dos dados dos formuários de referência CVM

--- appWeb/controllers/routes.py
+++ appWeb/controllers/routes.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 main = Blueprint('main', __name__)
 
+TABLE_NAMES_TITLE_PAGES = {
+    "cad_cia_aberta" : "Companhias Abertas (B3)",
+    "fre_cia_aberta_empregado_local_faixa_etaria_2024" : "Companhia Aberta Empregado Local Faixa Etária",
+    "fre_cia_aberta_empregado_local_declaracao_raca_2024" : "Companhia Aberta Empregado Local Declaração Raça",
+    "fre_cia_aberta_empregado_local_declaracao_genero_2024" : "Companhia Aberta Empregado Local Declaração Gênero"
+}
 
 # Função para conectar ao banco de dados
 def get_db_connection():
@@ -18,24 +24,29 @@ def index():
     return render_template('index.html')
 
 
-# Rota de visualização dos dados das companhias abertas B3
-@main.route('/view_companhias_abertas_b3')
-def view_companhias_abertas_b3():
+# Rota de visualização dos dados 
+@main.route('/view_data/<string:table_name>')
+def view_data(table_name):
     try:
     # Estabelece uma conexão com o banco de dados e realiza a query
         conn = get_db_connection()
-        query = "SELECT * FROM cad_cia_aberta limit 3"
+        query = f"SELECT * FROM {table_name} limit 3"
         df = pd.read_sql_query(query, conn)
         conn.close()
-    
-        return render_template("view_companhias_abertas_b3.html", table=df)
+        return render_template("view_companhias_abertas_b3.html", table=df, table_name=table_name, title_page=TABLE_NAMES_TITLE_PAGES[table_name])
 
     except Exception as e:
         return render_template("view_companhias_abertas_b3.html", error="Erro ao estabelecer conexão com banco de dados.")
 
 
-@main.route("/update_table/<name_table>/<int:row_id>", methods=["POST"])
-def update_table(name_table, row_id):
+@main.route('/form_ref_cvm')
+def form_ref_cvm():
+    title_page = 'Formulário de Referência (CVM)'
+    return render_template("form_ref_cvm.html", title_page=title_page)
+
+
+@main.route("/update_table/<table_name>/<int:row_id>", methods=["POST"])
+def update_table(table_name, row_id):
 
     dict_request = dict(request.form)
 
@@ -46,7 +57,7 @@ def update_table(name_table, row_id):
     
     # Remove a última vírgula 
     key_value = key_value[:-1]
-    query = f'UPDATE {name_table} SET {key_value} WHERE "index" = {row_id}'
+    query = f'UPDATE {table_name} SET {key_value} WHERE "index" = {row_id}'
 
     # Executar a query
     try:
@@ -63,44 +74,5 @@ def update_table(name_table, row_id):
         # Fechar a conexão
         conn.close()
 
-    return redirect(url_for('main.view_companhias_abertas_b3'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @main.route('/update', methods=['POST'])
-# def update():
-#     if request.method == 'POST':
-#         record_id = request.form['id']
-#         name = request.form['name']
-#         value = request.form['value']
-
-#         record = Record.query.get(record_id)
-#         if record:
-#             record.name = name
-#             record.value = float(value)
-#             db.session.commit()
-#             flash('Registro atualizado com sucesso!', 'success')
-#         else:
-#             flash('Registro não encontrado.', 'danger')
-
-#     return redirect(url_for('index'))
+    return redirect(url_for(f'main.view_data',table_name=table_name))
    
